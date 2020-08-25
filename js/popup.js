9f22aeec294c5f3e896b11af8bd9b836df7f218d
kowshik27 <kowshikkumar2704@gmail.com>
2020-08-25 23:29:11 +0530

    first commit

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,38 @@
-function popup1()
-{
+var openModalButtons = document.querySelectorAll('[data-modal-target]');
+var closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+    // console.log(modal);
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal);
+    //console.log(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
 }
 
-function popup2()
-{
-
-}
-function popup3()
-{
-
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
